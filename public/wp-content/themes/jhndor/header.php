9b5a3e7a1983730bef841950fbe91938ceb00ead
Johan Dornell <johan@Johans-MacBook-Air.local>
2017-11-14 19:37:49 +0100

    Add correct url to main css

--- public/wp-content/themes/jhndor/header.php
+++ public/wp-content/themes/jhndor/header.php
@@ -11,7 +11,7 @@
 
 	<title><?php echo get_bloginfo( 'name' ); ?></title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	<link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
+	<link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
